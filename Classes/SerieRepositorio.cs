0540dc3fc79bd0bfe5661286c4e56fa4e4ee2391
RafaelTDR <rafa.eel.tomazoni@gmail.com>
2021-11-25 22:10:32 -0300

    Cadastro de Series em memoria

---
+++ Classes/SerieRepositorio.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RTR.Series
+{
+    public class SerieRepositorio : Interfaces.IRepositorio<Serie>
+    {
+
+        private List<Serie> listaSerie = new List<Serie>();
+        public void Atualiza(int id, Serie entidade)
+        {
+            listaSerie[id] = entidade;
+        }
+
+        public void Exclui(int id)
+        {
+            listaSerie[id].Excluir();
+        }
+
+        public void Insere(Serie entidade)
+        {
+            listaSerie.Add(entidade);
+
+        }
+
+        public List<Serie> Lista()
+        {
+            return listaSerie;
+        }
+
+        public int ProximoId()
+        {
+            return listaSerie.Count;
+        }
+
+        public Serie RetornaPorId(int id)
+        {
+            return listaSerie[id];
+        }
+    }
+}
